67931ab5099fcc59d6ef0dd6e9aa2ea38b74e520
Georgiy Pruss <yru2l8@yahoo.com>
2017-12-15 01:48:33 +0200

    Minimized code a bit

--- s.nim
+++ s.nim
@@ -1,32 +1,25 @@
 # https://challenge.synacor.com/ - Dec'2017
 
-const LIMIT = 32768
+const LIMIT = 32768 # memory size, int limit
 const NR = 8 # number of registers
 
-var memory: array[LIMIT,int] # 16-bit value actually
-var reg: array[NR,int]
-var stack: seq[int] = @[] # stack, initially empty
+var M: array[LIMIT,int] # 15-bit value actually
+var R: array[NR,int]    # registers
+var S: seq[int] = @[]   # stack, initially empty
 
-proc read_into_memory( address: int, filename: string ): int =
+proc read_into_memory( address: int, filename: string ): int {. discardable .} =
   var a = address
   var b: int
-  var evenbyte = true
-  for c in readFile filename: # it reads chars
-    if evenbyte:
-      b = ord(c)
-    else:
-      memory[a] = (ord(c) shl 8) + b
-      a += 1
-    evenbyte = not evenbyte
+  for i,c in readFile filename: # it reads chars
+    if i mod 2 == 0: b = ord(c)
+    else: M[a] = (ord(c) shl 8) + b; a += 1
   return 2*(a-address)
 
-proc fill_memory( address: int, sz: int, value: int ) =
-  for a in address..<LIMIT:
-    memory[a] = value
+#proc fill_memory( address: int, sz: int, value: int ) =
+#  for a in address..<LIMIT:
+#    M[a] = value
 
-proc value( val: int ): int = # imm value or from register
-  if val<LIMIT: return val
-  return reg[val-LIMIT]
+proc value( v: int ): int = (if v<LIMIT: v else: R[v-LIMIT]) # imm value or from register
 
 var outbuf: string = ""
 var inbuf: string = ""
@@ -35,98 +28,80 @@ var inpos: int = 0 # next char index
 
 proc execute( pc: int ): int =
   # return <0 if must stop, otherwise it's next pc
-  let op = memory[pc]
+  let op = M[pc]
   case op:
   of 0:  # halt -- stop execution and terminate the program
     return -1
   of 1: # set a b -- set register <a> to the value of <b>
-    reg[memory[pc+1]-LIMIT] = value(memory[pc+2])
+    R[M[pc+1]-LIMIT] = value(M[pc+2])
     return pc+3
   of 2: # push a -- push <a> onto the stack
-    let c = value(memory[pc+1])
-    stack.add c
+    S.add value(M[pc+1])
     return pc+2
   of 3: # pop a -- remove the top element from the stack and write it into <a>; empty stack = error
-    if stack.len == 0: echo "pop from empty stack"; return -1
-    let c = stack[^1]
-    stack.del(stack.len-1)
-    reg[memory[pc+1]-LIMIT] = c
+    if S.len == 0: echo "pop from empty stack"; return -1
+    let c = S[^1]
+    S.del(S.len-1)
+    R[M[pc+1]-LIMIT] = c
     return pc+2
   of 4: # eq a b c -- set <a> to 1 if <b> is equal to <c>; set it to 0 otherwise
-    let v = value(memory[pc+2]) == value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = if v: 1 else: 0
+    R[M[pc+1]-LIMIT] = if value(M[pc+2]) == value(M[pc+3]): 1 else: 0
     return pc+4
   of 5: # gt a b c -- set <a> to 1 if <b> is greater than <c>; set it to 0 otherwise
-    let v = value(memory[pc+2]) > value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = if v: 1 else: 0
+    R[M[pc+1]-LIMIT] = if value(M[pc+2]) > value(M[pc+3]): 1 else: 0
     return pc+4
   of 6: # jmp a -- jump to <a>
-    return value(memory[pc+1]) # new pc
+    return value(M[pc+1]) # new pc
   of 7: # jt a b -- if <a> is nonzero, jump to <b>
-    if value(memory[pc+1]) != 0:
-      return value(memory[pc+2])
-    return pc+3
+    return if value(M[pc+1]) != 0: value(M[pc+2]) else: pc+3
   of 8: # jf a b -- if <a> is zero, jump to <b>
-    if value(memory[pc+1]) == 0:
-      return value(memory[pc+2])
-    return pc+3
+    return if value(M[pc+1]) == 0: value(M[pc+2]) else: pc+3
   of 9: # add a b c -- assign into <a> the sum of <b> and <c> (modulo 32768)
-    let v = value(memory[pc+2]) + value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = v mod LIMIT
+    R[M[pc+1]-LIMIT] = (value(M[pc+2]) + value(M[pc+3])) mod LIMIT
     return pc+4
   of 10: # mult a b c -- store into <a> the product of <b> and <c> (modulo 32768)
-    let v = value(memory[pc+2]) * value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = v mod LIMIT
+    R[M[pc+1]-LIMIT] = value(M[pc+2]) * value(M[pc+3]) mod LIMIT
     return pc+4
   of 11: # mod a b c -- store into <a> the remainder of <b> divided by <c>
-    let v = value(memory[pc+2]) mod value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = v
+    R[M[pc+1]-LIMIT] = value(M[pc+2]) mod value(M[pc+3])
     return pc+4
   of 12: # and a b c -- stores into <a> the bitwise and of <b> and <c>
-    let v = value(memory[pc+2]) and value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = v
+    R[M[pc+1]-LIMIT] = value(M[pc+2]) and value(M[pc+3])
     return pc+4
   of 13: # or a b c -- stores into <a> the bitwise or of <b> and <c>
-    let v = value(memory[pc+2]) or value(memory[pc+3])
-    reg[memory[pc+1]-LIMIT] = v
+    R[M[pc+1]-LIMIT] = value(M[pc+2]) or value(M[pc+3])
     return pc+4
   of 14: # not a b -- stores 15-bit bitwise inverse of <b> in <a>
-    let v = not value(memory[pc+2])
-    reg[memory[pc+1]-LIMIT] = v and 0x7FFF
+    R[M[pc+1]-LIMIT] = not value(M[pc+2]) and 0x7FFF
     return pc+3
-  of 15: # rmem a b -- read memory at address <b> and write it to <a>
-    let v = value(memory[pc+2])
-    let w = memory[v]
-    reg[memory[pc+1]-LIMIT] = w
+  of 15: # rmem a b -- read M at address <b> and write it to <a>
+    R[M[pc+1]-LIMIT] = M[value(M[pc+2])]
     return pc+3
-  of 16: # wmem a b -- write the value from <b> into memory at address <a>
-    let v = value(memory[pc+2])
-    let a = value(memory[pc+1])
-    memory[a] = v
+  of 16: # wmem a b -- write the value from <b> into M at address <a>
+    M[value(M[pc+1])] = value(M[pc+2])
     return pc+3
   of 17: # call a -- write the address of the next instruction to the stack and jump to <a>
-    stack.add pc+2
-    return value(memory[pc+1]) # new pc
+    S.add pc+2
+    return value(M[pc+1]) # new pc
   of 18: # ret -- remove the top element from the stack and jump to it; empty stack = halt
-    if stack.len == 0: echo "ret with empty stack"; return -1
-    let c = stack[^1]
-    stack.del(stack.len-1)
+    if S.len == 0: echo "ret with empty stack"; return -1
+    let c = S[^1]
+    S.del(S.len-1)
     return c # new pc
   of 19: # out a -- write the character represented by ascii code <a> to the terminal
-    let c = value(memory[pc+1])
-    if c==10: echo outbuf; outbuf = ""
-    else: outbuf &= chr(c)
+    let c = value(M[pc+1])
+    if c==10: echo outbuf; outbuf = "" else: outbuf &= chr(c)
     return pc+2
   of 20: # in a -- read a character from the terminal and write its ascii code to <a>
     # it can be assumed that once input starts, it will continue until a newline is encountered;
     # this means that you can safely read whole lines from the keyboard and trust that
     # they will be fully read
-    if incnt==-1 or (incnt>=0 and inpos>=incnt):
+    if incnt<0 or inpos>=incnt:
       inbuf = readLine(stdin)
+      if inbuf.len==0 or inbuf[^1]!='\10': inbuf &= '\10'
       incnt = inbuf.len
       inpos = 0
-    let a = value(memory[pc+1])
-    memory[a] = inbuf[inpos]
+    R[M[pc+1]-LIMIT] = inbuf[inpos].ord
     inpos += 1
     return pc+2
   of 21: # noop -- no operation
@@ -145,9 +120,9 @@ proc run( address: uint ) =
     echo outbuf; outbuf = ""
   echo "executed commands: ",cnt
 
-var sz = read_into_memory( 0, "challenge.bin" )
-#fill_memory( sz, LIMIT-sz, 0 )
+read_into_memory( 0, "challenge.bin" )
 run( 0 )
 
 # vhiYqHRHsOct - start breathing
 # QbtRsOmZrHKg - self-test passed
+# nQUUBNhKGafz - on tablet
